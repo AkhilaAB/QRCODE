9d2d46701111c9d5dbb496bb0ea13b78caad9e16
AkhilaEmergio <akhila@emergiotech.com>
2025-07-28 11:30:42 +0530

    code

--- qr.py
+++ qr.py
@@ -1,10 +1,10 @@
 import qrcode
 
 # Using address
-# address = "M4JQ+PR Erumapetty, Kerala"
+address = "RVM CONVENTION CENTRE, Erumapetty, Pazhiyottumuri, Kerala 680584"
 # address = "MG Road, Bengaluru, Karnataka"
-# maps_url = f"https://www.google.com/maps/search/?api=1&query={address.replace(' ', '+')}"
-maps_url = "https://www.google.com/maps/place/10%C2%B040'54.5%22N+76%C2%B008'22.2%22E/@10.681796,76.1395105,17z/data=!3m1!4b1!4m4!3m3!8m2!3d10.681796!4d76.1395105?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D"
+maps_url = f"https://www.google.com/maps/search/?api=1&query={address.replace(' ', '+')}"
+# maps_url = "https://www.google.com/maps/place/10%C2%B040'54.5%22N+76%C2%B008'22.2%22E/@10.681796,76.1395105,17z/data=!3m1!4b1!4m4!3m3!8m2!3d10.681796!4d76.1395105?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D"
 
 # Using latitude and longitude
 # latitude = 12.9716
